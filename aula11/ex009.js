15776f3473e98e1c1c72e05be44f2a74f485b21a
Levy <levisilvas2018@gmail.com>
2022-08-13 10:43:49 -0300

    exercicios aula 11

---
+++ aula11/ex009.js
@@ -0,0 +1,10 @@
+var idade = 21;
+
+console.log(`Eu tenho ${idade} anos!`);
+// condicao composta
+if(idade >= 18){
+    console.log("Voce pode tirar carta de habilitacao!");
+}
+else {
+    console.log("Infelizmente nao pode tirar sua carta de habilitacao!");
+}
---
+++ aula11/ex08.js
@@ -0,0 +1,8 @@
+var temp = 25.5;
+
+// condicao simples
+if(temp >= 25){
+    console.log("Vamos para a praia!");
+}
+
+console.log("Esta chegando as ferias!");
