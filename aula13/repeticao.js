88a5c8fd5edd1bef31a8912fb1feb4bad3580bb9
Levy <levisilvas2018@gmail.com>
2022-08-13 10:47:21 -0300

    aula sobre loops

---
+++ aula13/repeticao.js
@@ -0,0 +1,14 @@
+/* Comandos de repeticao */
+
+var count = 1;
+
+while(count <= 5){
+    console.log("*-*");
+    count++;
+}
+
+do {
+    console.log("********");
+    console.log("Welcome!");
+    count++;
+}while(count < 10)
